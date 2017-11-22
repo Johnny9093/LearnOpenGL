059b2d052caf9bfdd7c8aca366e33c98a5e07d72
yoni9093 <yoni9093@BMW23>
2017-11-22 16:18:21 +0200

    added vertex and index buffers, vertex array object, vertex and fragment shaders, and started drawing

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,22 +1,31 @@
-#include <glad\glad.h>
-#include <GLFW\glfw3.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #include <iostream>
-
-// Window constants
-const int WIDTH = 800;
-const int HEIGHT = 600;
-const char* TITLE = "LearnOpenGL";
 
 // Declaring the window resize callback function
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
-float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f,  0.5f, 0.0f
-};
+// Settings
+const int WIDTH = 800;
+const int HEIGHT = 600;
+const char* TITLE = "LearnOpenGL";
+
+// Shader code
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
 
 int main()
 {
@@ -40,19 +49,120 @@ int main()
 	// Make the context of our window the main context on the current thread
 	glfwMakeContextCurrent(window);
 
-	// Initialize GLAD with the OS-specific function to get function addresses, which is given by GLFW
+	// Registering the window resize callback function
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Initialize GLAD with the OS-specific function to get function addresses and load all OpenGL function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
-	// Initialize the viewport in which OpenGL will draw 
-	// Basically mapping OpenGLs' [-1, 1] coordinates to our windows' [0, WIDTH] and [0, HEIGHT] coordinates
-	glViewport(0, 0, 800, 600);
+	// Creating vertex shader
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
-	// Registering the window resize callback function
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	// Compiling
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// Error checking
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "Error: Vertex shader compilation failed." << std::endl;
+	}
+
+	// Creating vertex shader
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// Compiling
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// Error checking
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "Error: Fragment shader compilation failed." << std::endl;
+	}
+
+	// Creating shader program
+	unsigned int shaderProgram = glCreateProgram();
+
+	// Linking shaders to shader program
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// Error checking
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+	if (!success) 
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "Error: Shader program linkage failed." << std::endl;
+	}
+
+	// Deleting shader objects
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f, // bottom left
+		0.5f, -0.5f, 0.0f, // bottom right
+		-0.5f, 0.5f, 0.0f, // top left
+		0.5f,  0.5f, 0.0f // top right
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		1, 2, 3
+	};
+
+	unsigned int VAO, VBO, EBO;
+
+	// Generating vertex array object to hold our VBO configuration and attributes
+	glGenVertexArrays(1, &VAO);
+
+	// Generating vertex buffer
+	glGenBuffers(1, &VBO);
+
+	// Generating index buffer
+	glGenBuffers(1, &EBO);
+
+	// Binding the VAO to record the VBO data and vertex attributes configuration
+	glBindVertexArray(VAO);
+
+	// VBO
+
+	// Binding the VBO and copying the vertices to the vertex buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // GL_STATIC_DRAW means this data will change very rarely, if at all.
+
+	// Tell OpenGL how to read the VBO:
+	// We passed in vertices structured as an array of floats, where every triplet of floats is a vertex.
+	// This also registers the VBO as the vertex attribute's bound vertex buffer object
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// EBO
+
+	// Binding the EBO and copying the indices to the index buffer
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Unbind current buffer
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Unbind VAO
+	glBindVertexArray(0);
 
 	// Start render loop
 	while (!glfwWindowShouldClose(window))
@@ -63,6 +173,20 @@ int main()
 		// Rendering
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Start using the shader program with our defined shaders
+		glUseProgram(shaderProgram);
+
+		// Bind VAO to get the vertex attribute configuration and vertex data
+		glBindVertexArray(VAO);
+		
+		//// Draw vertices
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// Draw indexed vertices
+		// Bind the EBO and draw the indices
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// Check and call events and swap the buffers
 		// Swapping refers to the Double Buffer - read more at https://learnopengl.com/#!Getting-started/Hello-Window
@@ -77,6 +201,8 @@ int main()
 // Callback function to reset the viewport with the correct coordinates whenever a window resize occurrs
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+	// Initialize the viewport in which OpenGL will draw 
+	// Basically mapping OpenGLs' [-1, 1] coordinates to our windows' [0, WIDTH] and [0, HEIGHT] coordinates
 	glViewport(0, 0, width, height);
 }
 
