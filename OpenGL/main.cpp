94741d16a074aa78bbca27d86e72a915114d949f
John Lahav <yoni9093@gmail.com>
2018-04-19 19:06:13 +0300

    added glm

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -2,6 +2,10 @@
 #include <GLFW/glfw3.h>
 #include "stb_image.h"
 
+#include <glm\glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "Shader.h"
 
 #include <iostream>
